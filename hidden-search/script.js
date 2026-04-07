63957fac980107f86a7dfd79d63994bcd7d87ac2
luoyj46424 <2542111098@qq.com>
2026-04-08 00:59:26 +0800

    docs:【第 5 节： Day 4 - Hidden Search Widget】添加注释

--- hidden-search/script.js
+++ hidden-search/script.js
@@ -2,6 +2,7 @@ const search = document.querySelector('.search')
 const btn = document.querySelector('.btn')
 const input = document.querySelector('.input')
 
+// classList.toggle用来切换某个 CSS 类的存在状态：如果元素上没有指定的类，则添加该类。如果元素上已经有指定的类，则移除该类。
 btn.addEventListener('click', () => {
     search.classList.toggle('active')
     input.focus()
